a0304e340804686fb4aa66835bad0a106d0dd6fe
thenicealex <thenicealex4@gmail.com>
2023-08-26 18:45:08 +0800

    fix: telescope and add two plugins

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,9 +5,67 @@ return {
 	keys = {
 		{ "<leader>f", "<cmd>Telescope find_files<cr>" },
 		{ "<leader>b", "<cmd>Telescope buffers<cr>" },
-		{ "<leader>s", "<cmd>Telescope curren_buffer_fuzzy_find<cr>" },
+		{ "<leader>s", "<cmd>Telescope current_buffer_fuzzy_find<cr>" },
 		{ "<localleader>ss", "<cmd>Telescope live_grep<cr>" },
 		{ "<localleader>fh", "<cmd>Telescope help_tags<cr>" },
 		{ "<localleader>fa", "<cmd>Telescope find_files follow=true no_ignore=true hidden=true<cr>" },
 	},
+	opts = {
+		defaults = {
+			mappings = {
+				i = {
+					-- map actions.which_key to <C-h> (default: <C-/>)
+					-- actions.which_key shows the mappings for your picker,
+					-- e.g. git_{create, delete, ...}_branch for the git_branches picker
+					-- ["<C-h>"] = "which_key",
+				},
+			},
+		},
+		pickers = {
+			find_files = {
+				theme = "dropdown",
+				-- layout_strategy = "vertical",
+				previewer = false,
+				prompt_title = "",
+				prompt_prefix = "Files> ",
+			},
+			buffers = {
+				theme = "dropdown",
+				previewer = false,
+				prompt_title = "",
+				prompt_prefix = "Buffers> ",
+			},
+			live_grep = {
+				theme = "dropdown",
+				prompt_title = "",
+				preview_title = '',
+				prompt_prefix = "Global search> ",
+				layout_config = {
+					width = 0.8,
+					height = 0.3,
+				},
+			},
+			current_buffer_fuzzy_find = {
+				theme = "dropdown",
+				prompt_title = "",
+				preview_title = '',
+				prompt_prefix = "Search> ",
+				layout_config = {
+					width = 0.8,
+					height = 0.4,
+				},
+
+			},
+		},
+		extensions = {
+			-- Your extension configuration goes here:
+			-- extension_name = {
+			--   extension_config_key = value,
+			-- }
+			-- please take a look at the readme of the extension you want to configure
+		},
+	},
+	config = function(_, opts)
+		require("telescope").setup(opts)
+	end,
 }
--- lua/plugins/tools.lua
+++ lua/plugins/tools.lua
@@ -44,10 +44,68 @@ return {
     keys = {
       { "]t", function() require("todo-comments").jump_next() end, desc = "Next todo comment" },
       { "[t", function() require("todo-comments").jump_prev() end, desc = "Previous todo comment" },
-      { "<leader>xt", "<cmd>TodoTrouble<cr>", desc = "Todo (Trouble)" },
-      { "<leader>xT", "<cmd>TodoTrouble keywords=TODO,FIX,FIXME<cr>", desc = "Todo/Fix/Fixme (Trouble)" },
-      { "<leader>st", "<cmd>TodoTelescope<cr>", desc = "Todo" },
-      { "<leader>sT", "<cmd>TodoTelescope keywords=TODO,FIX,FIXME<cr>", desc = "Todo/Fix/Fixme" },
+      { "<localleader>xt", "<cmd>TodoTrouble<cr>", desc = "Todo (Trouble)" },
+      { "<localleader>xT", "<cmd>TodoTrouble keywords=TODO,FIX,FIXME<cr>", desc = "Todo/Fix/Fixme (Trouble)" },
+      { "<localleader>st", "<cmd>TodoTelescope<cr>", desc = "Todo" },
+      { "<localleader>sT", "<cmd>TodoTelescope keywords=TODO,FIX,FIXME<cr>", desc = "Todo/Fix/Fixme" },
     },
+	},
+	{
+		"windwp/nvim-spectre",
+		event = "BufRead",
+		config = function()
+			require("spectre").setup()
+		end,
+	},
+	{
+		"s1n7ax/nvim-window-picker",
+		version = "1.*",
+		keys = {
+			{
+				"n",
+				"<localleader>wp",
+				function()
+					local picker = require("window-picker")
+					local picked_window_id = picker.pick_window({
+						include_current_win = true,
+					}) or vim.api.nvim_get_current_win()
+					vim.api.nvim_set_current_win(picked_window_id)
+				end,
+				{ desc = "Pick a window" },
+			},
+				{
+					"n",
+					"<localleader>ws",
+					function()
+						local picker = require("window-picker")
+						local window = picker.pick_window({
+							include_current_win = false,
+						})
+						local target_buffer = vim.fn.winbufnr(window)
+						-- Set the target window to contain current buffer
+						vim.api.nvim_win_set_buf(window, 0)
+						-- Set current window to contain target buffer
+						vim.api.nvim_win_set_buf(0, target_buffer)
+					end,
+					{ desc = "Swap windows" },
+			},
+		},
+		config = function()
+			require("window-picker").setup({
+				autoselect_one = true,
+				include_current = false,
+				filter_rules = {
+					-- filter using buffer options
+					bo = {
+						-- if the file type is one of following, the window will be ignored
+						filetype = { "NvimTree","neo-tree", "neo-tree-popup", "notify", "quickfix" },
+
+						-- if the buffer type is one of following, the window will be ignored
+						buftype = { "terminal" },
+					},
+				},
+				other_win_hl_color = "#e35e4f",
+			})
+		end,
 	},
 }
